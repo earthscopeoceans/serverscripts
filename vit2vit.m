99b1ee8167c3e50330ef2a2f93451cebe7cf362d
Frederik J. Simons <fjsimons@gmail.com>
2020-03-03 09:20:47 -0500

    Returns deployment times for MERMAID floats, a MATLAB function

---
+++ vit2vit.m
@@ -0,0 +1,75 @@
+function begs=vit2vit
+% begs=vit2vit
+%
+% INPUT: None
+%
+% OUTPUT: 
+%
+% begs   A structure array with MERMAID name and deployment date
+%
+% Just a data base with deployment dates used by VIT2TBL
+% to take out lines from the *.vit files from when the instruments were
+% definitely not in the water yet
+%
+% Last modified by fjsimons-at-alum.mit.edu, 03/02/2020
+
+% KOBE MERMAIDS
+begs.N001=datetime(2018,12,27,01,07,34,'TimeZone','UTC');
+begs.N002=datetime(2018,12,28,15,49,41,'TimeZone','UTC');
+begs.N003=datetime(2019,01,01,22,39,13,'TimeZone','UTC');
+begs.N004=datetime(2019,01,03,00,29,44,'TimeZone','UTC');
+begs.N005=datetime(2019,01,04,01,05,46,'TimeZone','UTC');
+
+% GEOAZUR MERMAIDS
+begs.P006=datetime(2018,06,26,19,15,07,'TimeZone','UTC');
+begs.P007=datetime(2018,06,27,18,40,06,'TimeZone','UTC');
+
+% PRINCETON MERMAIDS, deployed by Yann
+begs.P008=datetime(2018,08,05,13,25,04,'TimeZone','UTC');
+begs.P009=datetime(2018,08,06,15,32,50,'TimeZone','UTC');
+begs.P010=datetime(2018,08,07,12,56,16,'TimeZone','UTC');
+begs.P011=datetime(2018,08,09,11,03,12,'TimeZone','UTC');
+begs.P012=datetime(2018,08,10,19,18,49,'TimeZone','UTC');
+% PRINCETON MERMAIDS, deployed by Frederik, %C for log-checked
+begs.P013=datetime(2018,08,31,17,02,48,'TimeZone','UTC'); %C
+begs.P016=datetime(2018,09,03,07,11,48,'TimeZone','UTC'); %C
+begs.P017=datetime(2018,09,04,11,04,02,'TimeZone','UTC'); %C
+begs.P018=datetime(2018,09,05,17,39,31,'TimeZone','UTC'); %C
+begs.P019=datetime(2018,09,06,20,11,04,'TimeZone','UTC'); %C
+begs.P020=datetime(2018,09,08,10,38,47,'TimeZone','UTC'); %C
+begs.P021=datetime(2018,09,09,17,43,45,'TimeZone','UTC'); %C
+begs.P022=datetime(2018,09,10,19,09,48,'TimeZone','UTC'); %C
+begs.P023=datetime(2018,09,12,02,15,16,'TimeZone','UTC'); %C
+begs.P024=datetime(2018,09,13,08,53,23,'TimeZone','UTC'); %C
+begs.P025=datetime(2018,09,14,11,58,11,'TimeZone','UTC'); %C
+
+% SUSTECH MERMAIDS
+begs.P026=datetime(2019,08,06,02,53,00,'TimeZone','UTC');
+begs.P027=datetime(2019
+begs.P028=datetime(2019
+begs.P029=datetime(2019
+begs.P031=datetime(2019
+begs.P032=datetime(2019
+begs.P033=datetime(2019
+begs.P034=datetime(2019
+begs.P035=datetime(2019
+begs.P036=datetime(2019
+begs.P037=datetime(2019
+begs.P038=datetime(2019
+begs.P039=datetime(2019
+begs.P040=datetime(2019
+begs.P041=datetime(2019
+begs.P042=datetime(2019
+begs.P043=datetime(2019
+begs.P044=datetime(2019
+begs.P045=datetime(2019
+begs.P046=datetime(2019
+begs.P046=datetime(2019
+begs.P048=datetime(2019
+begs.P049=datetime(2019
+
+% KOBE MERMAIDS
+begs.P050=datetime(2019
+begs.P052=datetime(2019
+begs.P053=datetime(2019
+begs.P054=datetime(2019
